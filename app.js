c913b2b9cca7245201c17a44422988e55b886010
Venkata Reddy Donthireddy <VDonthi@wabelhlp0509826.gsm1900.org>
2015-08-13 10:01:57 -0700

    CI/CD Demo

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ shipping_address_postal_code : node.shipping_address_postal_code,billing_address
     });
 });
 
-/*
+
 //Code Block
 
 app.get('/getallorderswith/:status', function(req, res) {
@@ -249,7 +249,7 @@ app.get('/getallorderswith/:status', function(req, res) {
              }
     });
 });
-*/
+
 
 app.all("/", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
